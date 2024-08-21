f124261ef42320806862fac4777a07806ef00db4
swamfire <usern5949@gmail.com>
2024-08-21 20:11:45 +0530

    Added profile picture handling

--- client/src/pages/profile/index.jsx
+++ client/src/pages/profile/index.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useAppStore } from "@/store";
 import { useNavigate } from "react-router-dom";
 import { IoArrowBack } from "react-icons/io5";
@@ -10,6 +10,7 @@ import { Button } from "@/components/ui/button";
 import { toast } from "sonner";
 import { apiClient } from "@/lib/api-client";
 import { UPDATE_PROFILE_ROUTE } from "@/utils/constants";
+import { ADD_PROFILE_IMAGE_ROUTE } from "../../utils/constants";
 
 function Profile() {
   const navigate = useNavigate();
@@ -19,6 +20,7 @@ function Profile() {
   const [image, setImage] = useState(null);
   const [hovered, setHovered] = useState(false);
   const [selectedColor, setSelectedColor] = useState(0);
+  const fileInputRef = useRef(null);
 
   useEffect(() => {
     if (userInfo.profileSetup) {
@@ -56,15 +58,56 @@ function Profile() {
           toast.success("Profile updated successfully");
           navigate("/chat");
         }
-      } catch (error) {}
+      } catch (error) {
+        console.log(error);
+      }
     }
   };
+
+  const handleNavigate = () => {
+    if (userInfo.profileSetup) {
+      navigate("/chat");
+    } else {
+      toast.error("Please setup profile.");
+    }
+  };
+
+  const handleFileInputClick = () => {
+    fileInputRef.current.click();
+  };
+
+  const handleImageChange = async (event) => {
+    const file = event.target.files[0];
+    console.log(file);
+    if (file) {
+      const formData = new FormData();
+      formData.append("profile-image", file);
+      const response = await apiClient.post(ADD_PROFILE_IMAGE_ROUTE, formData, {
+        withCredentials: true,
+      });
+      if (response.status == 200 && response.data.image) {
+        setUserInfo({ ...userInfo, image: response.data.image });
+        toast.success("Image updated successfully");
+      }
+      // const reader = new FileReader();
+      // reader.onload = () => {
+      //   console.log(reader);
+      //   setImage(reader.result);
+      // };
+      // reader.readAsDataURL(file);
+    }
+  };
+
+  const handleDeleteImage = async () => {};
 
   return (
     <div className="bg-[#1b1c24] h-[100vh] flex items-center justify-center flex-col gap-10">
       <div className="flex flex-col gap-10 w-[80vw] md:w-max">
         <div>
-          <IoArrowBack className="text-4xl lg:text-6xl text-white/90 cursor-pointer" />
+          <IoArrowBack
+            onClick={handleNavigate}
+            className="text-4xl lg:text-6xl text-white/90 cursor-pointer"
+          />
         </div>
         <div className="grid grid-cols-2">
           <div
@@ -92,7 +135,10 @@ function Profile() {
               )}
             </Avatar>
             {hovered && (
-              <div className="absolute inset-0 flex items-center justify-center bg-black/50 ring-fuchsia-50 rounded-full">
+              <div
+                onClick={image ? handleDeleteImage : handleFileInputClick}
+                className="absolute inset-0 flex items-center justify-center bg-black/50 ring-fuchsia-50 rounded-full"
+              >
                 {image ? (
                   <FaTrash className="text-white text-3xl cursor-pointer" />
                 ) : (
@@ -100,7 +146,14 @@ function Profile() {
                 )}
               </div>
             )}
-            {/* <input /> */}
+            <input
+              type="file"
+              ref={fileInputRef}
+              className="hidden"
+              onChange={handleImageChange}
+              name="profile-image"
+              accept=".png, .jpeg, .svg, .webp"
+            />
           </div>
           <div className="flex min-w-32 md:min-w-64 flex-col gap-5 text-white items-center justify-center ">
             <div className="w-full">
--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -1,6 +1,7 @@
 import User from "../models/UserModel.js";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
+import { renameSync, unlink } from "fs";
 
 const maxAge = 3 * 24 * 60 * 60 * 1000; //3days expiry
 const createToken = (email, userId) => {
@@ -102,6 +103,57 @@ export const getUserInfo = async (request, response, next) => {
 };
 
 export const updateProfile = async (request, response, next) => {
+    try {
+
+        const { userId } = request;
+        const { firstName, lastName, color } = request.body;
+        console.log(userId, firstName, lastName, color);
+        if (!firstName || !lastName) {
+            return response.status(404).send("Firstname, lastname and color is required");
+        }
+        const userData = await User.findByIdAndUpdate(userId, { firstName, lastName, color, profileSetup: true }, { new: true, runValidators: true });
+        return response.status(200).json({
+            id: userData.id,
+            email: userData.email,
+            firstName: userData.firstName,
+            lastName: userData.lastName,
+            color: userData.color,
+            image: userData.image,
+            profileSetup: userData.profileSetup
+        });
+    }
+    catch (error) {
+        console.log(error);
+        return response.status(500).send("Internal Server Error");
+    }
+};
+
+
+
+export const addProfileImage = async (request, response, next) => {
+    try {
+        if (!request.file) {
+            return response.status(400).send("File is required");
+        }
+
+        const date = Date.now();
+        console.log(request.file);
+        let fileName = "upload/profiles/" + date + "-" + request.file.originalname;
+        console.log(fileName);
+        renameSync(request.file.path, fileName);
+        // console.log("date", date);
+        // console.log("user ID", request.userId);
+        const updateUser = await User.findOneAndUpdate({ _id: request.userId }, { image: fileName }, { new: true, runValidators: true });
+        return response.status(200).json({ image: updateUser.image });
+    }
+    catch (error) {
+        console.log(error);
+        return response.status(500).send("Internal Server Error");
+    }
+};
+
+
+export const removeProfileImage = async (request, response, next) => {
     try {
 
         const { userId } = request;

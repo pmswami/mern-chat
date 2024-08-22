b44c087e65d60c340b237bd72901bb20d31b2e02
swamfire <usern5949@gmail.com>
2024-08-22 11:39:44 +0530

    created chat components

---
+++ client/src/pages/chat/components/chat-container/components/message-container/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function MessageContainer() {
+  return <div>MessageContainer</div>;
+}
+
+export default MessageContainer;
--- client/src/pages/chat/components/chat-container/index.jsx
+++ client/src/pages/chat/components/chat-container/index.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import ChatHeader from "./components/chat-header";
+import MessageBar from "./components/message-bar";
 
 function ChatContainer() {
-  return <div>ChatContainer</div>;
+  return (
+    <div className="flex fixed top-0 h-[100vh] w-[100vw] bg-[#1c1d25] flex-col md:static md:flex-1">
+      <ChatHeader />
+      <ChatContainer />
+      <MessageBar />
+    </div>
+  );
 }
 
 export default ChatContainer;

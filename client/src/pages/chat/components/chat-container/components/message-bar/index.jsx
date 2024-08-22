953283d7781f17ba4528cd3f89c36b1e1aaef657
swamfire <usern5949@gmail.com>
2024-08-22 16:26:07 +0530

    built chat window

--- client/src/pages/chat/components/chat-container/components/message-bar/index.jsx
+++ client/src/pages/chat/components/chat-container/components/message-bar/index.jsx
@@ -1,7 +1,70 @@
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
+import { GrAttachment } from "react-icons/gr";
+import { RiEmojiStickerLine } from "react-icons/ri";
+import { IoSend } from "react-icons/io5";
+import EmojiPicker from "emoji-picker-react";
 
 function MessageBar() {
-  return <div>MessageBar</div>;
+  const [message, setMessage] = useState("");
+  const emojiRef = useRef();
+  const [emojiPickerOpen, setEmojiPickerOpen] = useState(false);
+
+  const handleAddEmoji = (emoji) => {
+    setMessage((msg) => msg + emoji.emoji);
+  };
+
+  const handleSendMessage = async () => {};
+
+  useEffect(() => {
+    function handleClickOutside(event) {
+      if (emojiRef.current && !emojiRef.current.contains(event.target)) {
+        setEmojiPickerOpen(false);
+      }
+    }
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [emojiRef]);
+
+  return (
+    <div className="h-[10vh] bg:[#1c1d25] flex justify-center px-8 items-center mb-6 gap-6">
+      <div className="flex-1 flex bg-[#2a2b33] rounded-md items-center gap-5 pr-5">
+        <input
+          className="flex-1 p-5 bg-transparent rounded-md focus:border-none focus:outline-none"
+          type="text"
+          placeholder="Enter Message"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        />
+        <button className="text-neutral-500 focus:border-none focus:outline-none focus:text-white duration-300 transition-all">
+          <GrAttachment className="text-2xl" />
+        </button>
+        <div className="relative">
+          <button
+            className="text-neutral-500 focus:border-none focus:outline-none focus:text-white duration-300 transition-all"
+            onClick={() => setEmojiPickerOpen(true)}
+          >
+            <RiEmojiStickerLine className="text-2xl" />
+          </button>
+          <div className="absolute bottom-16 right-0" ref={emojiRef}>
+            <EmojiPicker
+              theme="dark"
+              open={emojiPickerOpen}
+              onEmojiClick={handleAddEmoji}
+              autoFocusSearch={false}
+            />
+          </div>
+        </div>
+      </div>
+      <button
+        className="bg-[#8417ff] rounded-md flex items-center justify-center p-5 hover:bg-[#741bda] focus:bg-[#741bda] text-neutral-500 focus:border-none focus:outline-none focus:text-white duration-300 transition-all"
+        onClick={handleSendMessage}
+      >
+        <IoSend className="text-2xl" />
+      </button>
+    </div>
+  );
 }
 
 export default MessageBar;
